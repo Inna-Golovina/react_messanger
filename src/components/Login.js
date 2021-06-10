d13ce7e88da220a02515e8c3dce4d8a3430421ec
Inna Golovina <igigig978@gmail.com>
2021-06-10 09:43:57 +0300

    second

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,9 +4,19 @@
 //import {Context} from "../index";
 //import firebase from "firebase";
 
+import { Container, Grid } from "@material-ui/core";
+
 
 const Login = () => {
-   
+  return (
+    <Container container
+      style={{height: window.innerHeight - 50}}
+    >
+      <Grid>
+
+      </Grid>
+    </Container>
+  )
 };
 
 export default Login;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
-//import React, {useContext} from 'react';
-//import AppBar from "@material-ui/core/AppBar";
-//import Toolbar from "@material-ui/core/Toolbar";
-//import {Button, Grid} from "@material-ui/core";
-//import {NavLink} from "react-router-dom";
-//import {LOGIN_ROUTE} from "../utils/consts";
+import React, {useContext} from 'react';
+import { Button, Grid } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../utils/consts';
 //import {Context} from "../index";
 //import {useAuthState} from "react-firebase-hooks/auth";
 
 const Navbar = () => {
+
+  const user = true;
+
     return (
-      <div>
-        Navbar
-      </div>
+      <AppBar color={"secondary"} position="static">
+        <Toolbar>
+          <Grid container justify={"flex-end"}>
+            {user 
+              ? <Button variant={"outlined"}>Выйти</Button>
+              : <NavLink to={LOGIN_ROUTE}>
+                  <Button variant={"outlined"}>Логин</Button>
+                </NavLink>
+            }
+          </Grid>
+    
+        </Toolbar>
+</AppBar>
     )
 };
 

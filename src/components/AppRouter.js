d49fb35781724c2bf23e335141a769af4c22b02a
Inna Golovina <igigig978@gmail.com>
2021-06-15 10:48:34 +0300

    fifth

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,7 +2,6 @@ import React, {useContext} from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom'
 import { privateRoutes, publicRoutes } from '../routes';
 import { CHAT_ROUTE, LOGIN_ROUTE } from '../utils/consts';
-//import {privateRoutes, publicRoutes} from "../routes";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {Context} from "../index";
 
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import {CHAT_ROUTE, LOGIN_ROUTE} from "./utils/consts";
 import Login from "./components/Login";
 import Chat from "./components/Chat";
 
-/* export const publicRoutes = [
+export const publicRoutes = [
     {
         path: LOGIN_ROUTE,
         Component: Login
@@ -14,19 +14,4 @@ export const privateRoutes = [
         path: CHAT_ROUTE,
         Component: Chat
     }
-]
- */
-
-export const publicRoutes = [
-  {
-    path: LOGIN_ROUTE,
-    Component: Login,
-  }
-]
-
-export const privateRoutes = [
-  {
-    path: CHAT_ROUTE,
-    Component: Chat,
-  }
 ]
